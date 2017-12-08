180391e83fb66a09eabcf3684b68755bce1205c4
ManasiShah1 <manasi.shah@comcast.net>
2017-12-07 18:27:46 -0800

    Completed Alliance and Team class

--- src/org/usfirst/frc/team1318/gamesim/event/Alliance.java
+++ src/org/usfirst/frc/team1318/gamesim/event/Alliance.java
@@ -1,5 +1,56 @@
 package org.usfirst.frc.team1318.gamesim.event;
 
 public class Alliance {
-
+	
+	private final Team team1;
+	private final Team team2;
+	private final Team team3;
+	
+	private Alliance(Team t1, Team t2, Team t3) {
+		this.team1 = t1;
+		this.team2 = t2;
+		this.team3 = t3;
+	}
+		
+	public Team getTeam1() {
+		return this.team1;
+	}
+	
+	public Team getTeam2() {
+		return this.team2;
+	}
+	
+	public Team getTeam3() {
+		return this.team3;
+	}
+	
+	public static class Builder {
+		
+		private Team team1 = new Team();
+		private Team team2 = new Team();
+		private Team team3 = new Team();
+		
+		public Builder() {
+			
+		}
+		
+		public Builder setTeam1(Team team1) {
+			this.team1 = team1;
+			return this;
+		}
+		
+		public Builder setTeam2(Team team2) {
+			this.team2 = team2;
+			return this;
+		}
+		
+		public Builder setTeam3(Team team3) {
+			this.team3 = team3;
+			return this;
+		}
+		
+		public Alliance build() {
+			return new Alliance(team1, team2, team3);
+		}
+	}
 }
--- src/org/usfirst/frc/team1318/gamesim/event/Team.java
+++ src/org/usfirst/frc/team1318/gamesim/event/Team.java
@@ -1,5 +1,45 @@
 package org.usfirst.frc.team1318.gamesim.event;
 
 public class Team {
-
+	
+	private final Robot robot;
+	private final HumanPlayer player;
+	
+	private Team(Robot robot, HumanPlayer player) {
+		this.robot = robot;
+		this.player = player;
+	}
+	
+	public Robot getRobot() {
+		return this.robot;
+	}
+	
+	public HumanPlayer getPlayer() {
+		return this.player;
+	}
+	
+	public static class Builder {
+		
+		private Robot robot = new Robot();
+		private HumanPlayer player = new HumanPlayer();
+		
+		public Builder() {
+			
+		}
+		
+		// why do we have to return Builder?
+		public Builder setRobot(Robot robot) {
+			this.robot = robot;
+			return this;
+		}
+		
+		public Builder setPlayer(HumanPlayer player) {
+			this.player = player;
+			return this;
+		}
+		
+		public Team build() {
+			return new Team(robot, player);
+		}
+	}
 }
