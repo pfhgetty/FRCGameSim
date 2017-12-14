665961405292575f7c9c96172fa242db468f53e3
Aedan <aedan.smith@gmail.com>
2017-12-13 16:28:01 -0800

    Replace sealed objects with enums

--- src/main/scala/org/usfirst/irs1318/gamesim/engine/actions/Direction.scala
+++ src/main/scala/org/usfirst/irs1318/gamesim/engine/actions/Direction.scala
@@ -1,10 +1,6 @@
 package org.usfirst.irs1318.gamesim.engine.actions
 
-sealed class Direction
-
-object Direction {
-  case object Up extends Direction
-  case object Down extends Direction
-  case object Left extends Direction
-  case object Right extends Direction
+class Direction
+object Direction extends Enumeration {
+  val UP, DOWN, LEFT, RIGHT = Value
 }
--- src/main/scala/org/usfirst/irs1318/gamesim/engine/actors/Field.scala
+++ src/main/scala/org/usfirst/irs1318/gamesim/engine/actors/Field.scala
@@ -6,16 +6,11 @@ case class Field(width: Int,
                  height: Int,
                  phase: Field.Phase,
                  location: Location,
-                 containers: Map[String, Containable]) extends Actor {
-
-}
+                 containers: Map[String, Containable]) extends Actor
 
 object Field {
-  sealed class Phase
-  object Phase {
-    case object Pre extends Phase
-    case object Auto extends Phase
-    case object Teleop extends Phase
-    case object Post extends Phase
+  class Phase
+  object Phase extends Enumeration {
+    val PRE, AUTO, TELEOP, POST = Value
   }
 }
