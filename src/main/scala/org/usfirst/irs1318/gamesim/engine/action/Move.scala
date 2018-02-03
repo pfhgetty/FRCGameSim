398cb5ebf3ed6322fb0217b2e900dcde0e14c3bc
aedans <aedan.smith@outlook.com>
2018-02-03 01:18:00 -0800

    Fix move

--- src/main/scala/org/usfirst/irs1318/gamesim/engine/action/Move.scala
+++ src/main/scala/org/usfirst/irs1318/gamesim/engine/action/Move.scala
@@ -5,5 +5,5 @@ import org.usfirst.irs1318.gamesim.engine.actors._
 
 case class Move(name: String, direction: Direction) extends Action {
   override def run(state: GameEngine.State): GameEngine.State = state
-    .mapActor(name, _.mapLocation(_.translate(direction)))
+    .mapActor(name, (_: Actor).mapLocation(_.translate(direction)))
 }
